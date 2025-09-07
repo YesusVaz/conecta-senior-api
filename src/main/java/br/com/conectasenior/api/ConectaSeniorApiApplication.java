f2b5a92b2d056b05653608ee2512062599fe911b
YesusVaz <yesuslucas@gmail.com>
2025-09-07 18:14:40 -0300

    First commit

---
+++ src/main/java/br/com/conectasenior/api/ConectaSeniorApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.conectasenior.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaSeniorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaSeniorApiApplication.class, args);
+	}
+
+}
